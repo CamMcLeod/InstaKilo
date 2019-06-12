76f78ff0463627635ff5138d645eea30cf18a3ab
Cam McLeod <cammcleod3@gmail.com>
2019-06-12 11:42:35 -0700

    Initial Commit

---
+++ InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKilo
+//
+//  Created by Cameron Mcleod on 2019-06-12.
+//  Copyright © 2019 Cameron Mcleod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaKilo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  InstaKilo
+//
+//  Created by Cameron Mcleod on 2019-06-12.
+//  Copyright © 2019 Cameron Mcleod. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
